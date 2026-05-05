19941289a5041d5499258078305131fbc6f2792c
xoyzo <chukwubuikemc.alt@gmail.com>
2026-05-05 19:10:49 -0400

    Create apps.py

---
+++ rng_events_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class RNGEventsSystemConfig(AppConfig):
+    name = "rng_events_system"
+    verbose_name = "RNG Events System"
+    dpy_package = "rng_events_system.ext"
